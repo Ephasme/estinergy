4b77a4afe1b0eb4c25d6c9e4f9b92f7f144e7b10
Ephasme <loup.peluso@gmail.com>
2024-06-04 18:26:30 +0200

    --wip-- [skip ci]

--- src/hooks/useReset.ts
+++
@@ -1,9 +0,0 @@
-import { appliancesWithConsumptionAtom } from "@/store/atoms";
-import { useSetAtom } from "jotai";
-
-export const useReset = () => {
-  const setList = useSetAtom(appliancesWithConsumptionAtom);
-  return () => {
-    setList(null);
-  };
-};
